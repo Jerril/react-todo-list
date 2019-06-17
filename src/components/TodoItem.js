349ca3f8405778e1dc1554a19de847270a2fd088
Jerril <30305623+Jerril@users.noreply.github.com>
2019-06-17 23:37:26 +0530

    complete-final app

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -7,11 +7,11 @@ class TodoItem extends Component {
       <li className="list-group-item text-capitalize my-2 d-flex justify-content-between">
         <h6>{title}</h6>
         <div className="todo-ico">
-          <span className="mx-2 txt-success" onClick={handleEdit}>
-            <i className="fas fa-pen" />e
+          <span className="mx-2 text-success" onClick={handleEdit}>
+            <i className="fas fa-pen" />
           </span>
-          <span className="mx-2 txt-danger" onClick={handleDelete}>
-            <i className="fas fa-trash" />d
+          <span className="mx-2 text-danger" onClick={handleDelete}>
+            <i className="fas fa-trash" />
           </span>
         </div>
       </li>
